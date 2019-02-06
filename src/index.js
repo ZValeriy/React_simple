00de2e757eea10f59f6923743c8b45a2322f53bb
Valeriy <valeriyzubairov@gmail.com>
2019-02-06 04:10:25 +0300

    adding info about myself

--- src/index.js
+++ src/index.js
@@ -1,14 +1,6 @@
 import React from "react"
 import ReactDOM from "react-dom"
 
+import MyInfo from "../components/MyInfo.js"
 
-function MyApp() {
-    return (
-        <ul>
-            <li>1</li>
-            <li>2</li>
-            <li>3</li>
-        </ul>
-        )
-}
-ReactDOM.render(<MyApp />, document.getElementById("root"))
+ReactDOM.render(<MyInfo />, document.getElementById("root"))
