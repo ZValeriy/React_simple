7af46d301905319411e93223f56d71fa8be72f7f
Valeriy <valeriyzubairov@gmail.com>
2019-02-07 02:22:43 +0300

    Add styles, Item to component

--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -1,20 +1,14 @@
 import React from "react"
-import MyInfo from "./MyInfo";
+import ToDoItem from "./ToDoItem";
 
 function MainContent() {
     return (
-        <div>
-            <input type="checkbox" />
-            <p>Inp1</p>
-
-            <input type="checkbox" />
-            <p>Inp2</p>
-
-            <input type="checkbox" />
-            <p>Inp3</p>
-
-            <input type="checkbox" />
-            <p>Inp4</p>
+        <div className="todo-list">
+            <ToDoItem />
+            <ToDoItem />
+            <ToDoItem />
+            <ToDoItem />
+            <ToDoItem />
         </div>
     )
 }
