9e6fa1c044ef92b098406409ae762e510185f84b
Valeriy <valeriyzubairov@gmail.com>
2019-02-08 15:13:04 +0300

    1-st commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,37 @@
-import React from 'react';
-import Footer from './components/Footer';
-import Header from './components/Header';
-import MainContent from './components/MainContent';
+import React from "react"
+
 function App() {
-  return (
-    <div>
-      <Header />
-      <MainContent />
-      <Footer />
-    </div>
-  )
+    return (
+        <div className="contacts">
+            <div className="contact-card">
+                <img align="center" src="http://placekitten.com/300/200"/>
+                <h3><font color="#3AC1EF">▍Mr. Whiskerson</font></h3>
+                <p>Phone: (212) 555-1234</p>
+                <p>Email: mr.whiskaz@catnap.meow</p>
+            </div>
+            
+            <div className="contact-card">
+                <img align="center" src="http://placekitten.com/400/200"/>
+                <h3><font color="#3AC1EF">▍Fluffykins</font></h3>
+                <p>Phone: (212) 555-2345</p>
+                <p>Email: fluff@me.com</p>
+            </div>
+            
+            <div className="contact-card">
+                <img align="center" src="http://placekitten.com/400/300"/>
+                <h3><font color="#3AC1EF">▍Destroyer</font></h3>
+                <p>Phone: (212) 555-3456</p>
+                <p>Email: ofworlds@yahoo.com</p>
+            </div>
+            
+            <div className="contact-card">
+                <img align="center" src="http://placekitten.com/200/100"/>
+                <h3><font color="#3AC1EF">▍Felix</font></h3>
+                <p>Phone: (212) 555-4567</p>
+                <p>Email: thecat@hotmail.com</p>
+            </div>
+        </div>
+    )
 }
 
-export default App;
+export default App
