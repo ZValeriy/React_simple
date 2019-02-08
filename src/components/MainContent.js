8642b1419d4ba5adb3c6b5abcd4de48293a3bb6f
Valeriy <valeriyzubairov@gmail.com>
2019-02-09 00:37:06 +0300

    Add Changing state

--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -8,10 +8,25 @@ class MainContent extends Component {
         this.state = {
             todos: todosData
         }
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleChange(id, prevState) {
+        this.setState(prevState => {
+            const newTodos =  prevState.todos.map(item => {
+                if(item.id === id) {
+                    item.completed = !item.completed
+                }
+                return item;
+            });
+            return {
+                todos: newTodos
+            }
+        })
     }
 
     render() {
-        const todoItems = this.state.todos.map(item => <ToDoItem key={item.id} item={item}/>)
+        const todoItems = this.state.todos.map(item => <ToDoItem key={item.id} item={item} handleChange={this.handleChange}/>)
     return (
         <div className="todo-list">
         {todoItems}
